9d484019d2c936e4ac4e4ec2e32c6c09e63e6b91
M7zain <zeynkara91@gmail.com>
2024-11-27 19:10:14 +0300

    changed name of home route to Home because its case sensitive

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -18,7 +18,7 @@ const Sidebar = ({ toggle }) => {
         <ul className="space-y-2 font-medium">
           <li>
             <Link
-              href="/home/generate"
+              href="/Home/generate"
               className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
               <EventIcon />
