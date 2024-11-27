eac98c4b0db647e24f09075e07c033f648a0e55f
M7zain <zeynkara91@gmail.com>
2024-11-27 11:59:44 +0300

    optimized navbar component

--- src/app/Home/layout.jsx
+++ src/app/Home/layout.jsx
@@ -1,17 +1,42 @@
+'use client'
+import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 
 export default function Layout({ children }) {
+
+  const [toggle , setToggle ] = useState(false); 
+
+  
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 640) {
+        // Automatically open the sidebar on larger screens
+        setToggle(true);
+      } else {
+        // Keep the sidebar closed on smaller screens
+        setToggle(false);
+      }
+    };
+
+    // Add event listener for resizing
+    window.addEventListener("resize", handleResize);
+
+    // Initial check
+    handleResize();
+
+    // Cleanup the event listener
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-
     <>
-   <Navbar/>
+      <Navbar toggle={toggle} setToggle={setToggle} />
 
-   <Sidebar/>
+      <Sidebar toggle={toggle} />
 
-   <div className="p-4 sm:ml-64">
-      {children}
-    </div>
-   </>
+      <div className="p-4 sm:ml-64">{children}</div>
+    </>
   );
 }
--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -1,43 +1,41 @@
-import { SignOutButton } from "@clerk/nextjs";
 import Link from "next/link";
-import React from "react";
 import EventIcon from "./icons/EventIcon";
 import SignOutIcon from "./icons/SignoutIcon";
+import { SignOutButton } from "@clerk/nextjs";
 
-const Sidebar = () => {
+const Sidebar = ({ toggle }) => {
+
+
   return (
-    <>
-      <aside
-        id="logo-sidebar"
-        className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
-        aria-label="Sidebar"
-      >
-        <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
-          <ul className="space-y-2 font-medium">
-            <li>
-              <Link
-                href="/home/generate"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-              >
-                <EventIcon />
-                <span className="ms-3">Events</span>
-              </Link>
-            </li>
-
-            <li>
-              <SignOutButton>
-                <p className="flex items-center p-2 hover:cursor-pointer text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                  <SignOutIcon />
-                  <span className="flex-1 ms-3 whitespace-nowrap">
-                    Sign Out
-                  </span>
-                </p>
-              </SignOutButton>
-            </li>
-          </ul>
-        </div>
-      </aside>
-    </>
+    <aside
+      id="logo-sidebar"
+      className={`fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform ${
+        toggle ? "translate-x-0" : "-translate-x-full"
+      } bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700`}
+      aria-label="Sidebar"
+    >
+      <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+        <ul className="space-y-2 font-medium">
+          <li>
+            <Link
+              href="/home/generate"
+              className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+            >
+              <EventIcon />
+              <span className="ms-3">Events</span>
+            </Link>
+          </li>
+          <li>
+            <SignOutButton>
+              <p className="flex items-center p-2 hover:cursor-pointer text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <SignOutIcon />
+                <span className="flex-1 ms-3 whitespace-nowrap">Sign Out</span>
+              </p>
+            </SignOutButton>
+          </li>
+        </ul>
+      </div>
+    </aside>
   );
 };
 
